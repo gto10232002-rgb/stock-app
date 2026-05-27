f438ea1b07277406ca5d64899ffe787c4dfc8fa3
gto10232002-rgb <gto10232002@gmail.com>
2026-05-28 02:35:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ st.markdown("### 📊 台股籌碼選股")
 
 @st.cache_data(ttl=3600)
 def get_stock_data():
+    # 下載股價與籌碼資料
     url_price = "https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL"
     res_price = requests.get(url_price, timeout=20)
     df_price = pd.DataFrame()
@@ -43,7 +44,7 @@ def get_stock_data():
     df['chip_ratio'] = ((df['fi'] + df['it']) / df['vol'] * 100).round(2)
     return df
 
-# 主邏輯區塊：嚴格遵守縮排與括號閉合
+# 主執行區塊
 try:
     df = get_stock_data()
     st.sidebar.header("篩選條件")
@@ -52,11 +53,12 @@ try:
     min_v = st.sidebar.number_input("最低成交量(張)", value=1000)
     min_c = st.sidebar.slider("最低籌碼集中度(%)", -50, 50, 5)
     
+    # 篩選邏輯
     res = df[(df['price'] >= min_p) & (df['price'] <= max_p) & 
              (df['vol'] >= min_v) & (df['chip_ratio'] >= min_c)].copy()
     res = res.sort_values(by='chip_ratio', ascending=False)
     
-    # 建立合併欄位
+    # 合併資訊欄位
     res['個股資訊'] = "https://tw.stock.yahoo.com/quote/" + res['code'] + " " + res['code'] + " " + res['name']
     
     display_df = res[['個股資訊', 'price', 'chip_ratio']].rename(
@@ -65,11 +67,14 @@ try:
     
     st.write("📈 符合條件：" + str(len(display_df)) + " 檔")
     
-    # 顯示表格，LinkColumn 確保 Regex 準確
+    # 顯示表格，配置 LinkColumn 以隱藏 URL
     st.dataframe(
         display_df,
         column_config={
             "個股資訊": st.column_config.LinkColumn(
                 "代號/名稱", 
                 help="點擊前往 Yahoo 股市",
-                display_text=r"
+                display_text=r"https://tw.stock.yahoo.com/quote/\d+ (.*)"
+            ),
+            "股價": st.column_config.NumberColumn(format="%.2f"),
+            "集中度%": st.column_config.NumberColumn
